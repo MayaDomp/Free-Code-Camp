71ef78ef62f2d95f2402b960bb65753a0ec21321
MayaDomp <Dompierremaya83@gmail.com>
2025-05-27 16:27:50 -0400

    fullhelmet

--- myApp.js
+++ myApp.js
@@ -1,24 +1,29 @@
+// variables stuff
+
 const express = require('express');
-const helmet = require('helmet');
 const app = express();
+const helmet = require('helmet');
 const ninetyDaysInSeconds = 90*24*60*60;
 let timeInSeconds = ninetyDaysInSeconds;
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc:["'self'"],
-    scriptSrc:["'self'",'trusted-cdn.com'],
+// app stuff
 
-  }
-}));
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'",'trusted-cdn.com'],
+    }
+  },
+  dnsPrefetchControl: false,
+  hsts: {force: true},
+  noCache: true,
+}))
+
+
 
 
 
