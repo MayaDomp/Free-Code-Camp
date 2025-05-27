a469d49292cef831651d5e05d32a247c8a187c81
MayaDomp <Dompierremaya83@gmail.com>
2025-05-27 13:16:55 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,9 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('Info Sec App Started on port ${PORT}');
-});
+
 
 
 
@@ -66,6 +64,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log('Info Sec App Started on port ${PORT}');
 });
